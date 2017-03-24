3db128bf04a0431f01daa38a9f98f2dcd0abbdf2
Antsa Randriamihaja <arandriamihaja@gmail.com>
2017-03-23 20:30:40 -0400

    server files added

---
+++ server/models/todo.js
@@ -0,0 +1,29 @@
+//create a todo model with text and completed attribute => so mongoose knows how to store our data
+var mongoose = require('mongoose');
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true //removes empty space at beiginning and end of string
+    }, completed:{
+        type: Boolean,
+        default: false
+    }, completedAt :{
+        type: Number,
+        default: null
+    }
+});
+
+//create an instance of Todo which is now a constructor function
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+// //save newTodo in a database
+// //save() returns a promise
+// newTodo.save().then((doc)=>{
+//     console.log(`Saved Todo: ${doc}`);
+// },(e)=>{
+//     console.log('Unable to save Todo');
+// });
+module.exports = {Todo};
---
+++ server/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+//Configure middleware
+
+app.use(bodyParser.json());//returns a function which is the middleware we need to give to express
+//creating new todos => routes
+//Configure routes: POST route to crete new todos
+//basic API: operations CRUD: Create, Read, Update, Delete
+//To create a resource, you use the POST http method, send resource as the body.
+//Set up a route:
+app.post('/todos', (req, res)=>{
+     //body is stored by bodyParser.url we want to send our data to: localhost:3000/todos
+     var todo = new Todo({
+         text: req.body.text
+     });
+     todo.save().then((doc)=>{
+         res.send(doc);
+     }, (e)=>{
+         res.status(400).send(e);
+     });
+});
+
+//url for rest API is really important. For resource cretion: url: '/todos'
+app.listen(3000, ()=>{
+    console.log('Started on port 3000');
+});
